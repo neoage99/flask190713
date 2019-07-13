3e98492ef3d8ac9a7349a3d37a44f2c97f629a1f
neoage <neoage@gmail.com>
2019-07-13 16:37:53 +0900

    no message

--- imdb_test/model.py
+++ imdb_test/model.py
@@ -1,4 +1,7 @@
 import tensorflow_datasets as tfds
+import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
 
 class Imdb:
     def __init__(self):
@@ -15,6 +18,15 @@ class Imdb:
 
         train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
 
-        print('10개의 샘플 %s'% (train_examples_batch))
-        print('10개의 라벨 %s'% (train_labels_batch))
-        
+        #print('10개의 샘플 %s'% (train_examples_batch))
+        #print('10개의 라벨 %s'% (train_labels_batch))
+
+        embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+        hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                                   dtype=tf.string, trainable=True)
+        hub_layer(train_examples_batch[:3])
+
+        model = tf.keras.Sequential()
+        model.add(hub_layer)
+        model.add(tf.keras.layers.Dense(16,activation='relu'))
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
--- sequential_model/model.py
+++ sequential_model/model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class SeqModel:
 
@@ -19,21 +21,32 @@ class SeqModel:
        Non-trainable params: 0
        """
 
-    def compile_train(self):
-        self.model.compile(optimizer = 'sgd', loss = 'mse')
+    def execute(self):
         (x, y) = self.make_random_data()
-        history = self.model.fit(x, y, epochs = 500, validation_split = 0.3)
-        return [x,y,history]
+        x_train, y_train = x[:150], y[:150]
+        x_test, y_test = x[:150], y[:150]
+        self.model = tf.keras.Sequential()
+        self.model.add(tf.keras.layers.Dense(units=1, input_dim=1))
+        self.model.summary()
+
+        self.model.compile(optimizer = 'sgd', loss = 'mse')
+
+        history = self.model.fit(x_train, y_train, epochs = 50, validation_split = 0.3)
+        epochs = np.arange(1, 50+1)
+        plt.plot(epochs, history.history['loss'], label = 'Training loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
 
     @staticmethod
     def make_random_data():
-        x = np.random.uniform(low=-2, high=2, size=200)
+        x = np.random.uniform(low = -2, high=2, size = 200)
         y = []
         for t in x:
-            r = np.random.normal(loc=0.0,
+            r = np.random.normal(loc = 0.0,
                                  scale=(0.5 + t*t/3),
-                                 size= None)
+                                 size = None)
             y.append(r)
-        history = x, 1.726*x - 0.84 + np.array(y)
-        arr = [x, y, history]
-        return arr
+
+        return x, 1.726*x - 0.84 + np.array(y)
